6ca517de72a0426a37ad8e54106dc80d6d4ce76c
lizhuyang <lizhuyang@baidu.com>
2017-04-25 19:05:02 +0800

    optimize from redis

--- src/main/java/alchemystar/core/codec/RedisCmdDecoder.java
+++ src/main/java/alchemystar/core/codec/RedisCmdDecoder.java
@@ -9,7 +9,7 @@ import io.netty.handler.codec.ReplayingDecoder;
 
 /**
  * RedisCmdDecoder
- * todo singleTon
+ *
  * @Author lizhuyang
  */
 public class RedisCmdDecoder extends ReplayingDecoder {
--- src/main/java/alchemystar/core/protocol/cmd/RedisCmd.java
+++ src/main/java/alchemystar/core/protocol/cmd/RedisCmd.java
@@ -24,6 +24,36 @@ public class RedisCmd {
     private byte[] command;
     private byte[][] args;
 
+
+    //Jedis Code
+    private final static int[] sizeTable = { 9, 99, 999, 9999, 99999, 999999,
+            9999999, 99999999, 999999999, Integer.MAX_VALUE };
+
+    private final static byte[] DigitTens = { '0', '0', '0', '0', '0', '0',
+            '0', '0', '0', '0', '1', '1', '1', '1', '1', '1', '1', '1', '1',
+            '1', '2', '2', '2', '2', '2', '2', '2', '2', '2', '2', '3', '3',
+            '3', '3', '3', '3', '3', '3', '3', '3', '4', '4', '4', '4', '4',
+            '4', '4', '4', '4', '4', '5', '5', '5', '5', '5', '5', '5', '5',
+            '5', '5', '6', '6', '6', '6', '6', '6', '6', '6', '6', '6', '7',
+            '7', '7', '7', '7', '7', '7', '7', '7', '7', '8', '8', '8', '8',
+            '8', '8', '8', '8', '8', '8', '9', '9', '9', '9', '9', '9', '9',
+            '9', '9', '9', };
+
+    private final static byte[] DigitOnes = { '0', '1', '2', '3', '4', '5',
+            '6', '7', '8', '9', '0', '1', '2', '3', '4', '5', '6', '7', '8',
+            '9', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '1',
+            '2', '3', '4', '5', '6', '7', '8', '9', '0', '1', '2', '3', '4',
+            '5', '6', '7', '8', '9', '0', '1', '2', '3', '4', '5', '6', '7',
+            '8', '9', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0',
+            '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '1', '2', '3',
+            '4', '5', '6', '7', '8', '9', '0', '1', '2', '3', '4', '5', '6',
+            '7', '8', '9', };
+
+    private final static byte[] digits = { '0', '1', '2', '3', '4', '5', '6',
+            '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j',
+            'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w',
+            'x', 'y', 'z' };
+
     public RedisCmd(String command, List<String> args) {
         commandStr = command;
         argsStr = args;
@@ -102,13 +132,41 @@ public class RedisCmd {
         writeCRLF(byteBuf);
     }
 
+    // From Jedis Code
     public static void writeInt(int value, ByteBuf byteBuf) {
         // 如果<0,需要写入-|value|
         if (value < 0) {
             byteBuf.writeByte(RedisProtoConst.MINUS);
             value = -value;
         }
-        byteBuf.writeBytes(String.valueOf(value).getBytes());
+
+        int size = 0;
+        while (value > sizeTable[size])
+            size++;
+
+        size++;
+
+        int q, r;
+        int charPos = size;
+        byte[] buf = new byte[charPos];
+
+        while (value >= 65536) {
+            q = value / 100;
+            r = value - ((q << 6) + (q << 5) + (q << 2));
+            value = q;
+            buf[--charPos] = DigitOnes[r];
+            buf[--charPos] = DigitTens[r];
+        }
+
+        for (;;) {
+            q = (value * 52429) >>> (16 + 3);
+            r = value - ((q << 3) + (q << 1));
+            buf[--charPos] = digits[r];
+            value = q;
+            if (value == 0)
+                break;
+        }
+        byteBuf.writeBytes(buf);
     }
 
     public static void writeCRLF(ByteBuf byteBuf) {
